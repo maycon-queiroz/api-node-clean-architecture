1326122d9c0c20e0bedab9eb6e6e029b77fbc7c5
Maycon Queiroz <fmaycon@gmail.com>
2022-04-21 22:05:09 -0300

    ensure EmailValidator return false if Validator returns is false

--- src/presentation/utils/email-validator.spec.js
+++ src/presentation/utils/email-validator.spec.js
@@ -1,14 +1,23 @@
+const validator = require('validator')
 class EmailValidator {
   isValid (email) {
-    return true
+    return validator.isEmail(email)
   }
 }
 
 describe('Email Validator', () => {
-  test('should return true if returns is true', () => {
+  test('should return true if Validator returns is true', () => {
     const sut = new EmailValidator()
-    const isEmailValid = sut.isValid('valid_emnail@gmail.com')
+    const isEmailValid = sut.isValid('valid_email@gmail.com')
 
     expect(isEmailValid).toBe(true)
+  })
+
+  test('should return false if Validator returns is false', () => {
+    const sut = new EmailValidator()
+    validator.isEmailValid = false
+    const isEmailValid = sut.isValid('invalid_email@gmail')
+
+    expect(isEmailValid).toBe(false)
   })
 })
