1d95bfcfd9168ce9deb07f3664b90978fed714fb
Maycon Queiroz <fmaycon@gmail.com>
2022-04-21 22:12:20 -0300

    ensure EmailValidator call validator with correct email

--- src/presentation/utils/email-validator.spec.js
+++ src/presentation/utils/email-validator.spec.js
@@ -10,18 +10,26 @@ const makeSut = () => {
 }
 
 describe('Email Validator', () => {
-  test('should return true if Validator returns is true', () => {
+  test('Should return true if Validator returns is true', () => {
     const sut = makeSut()
     const isEmailValid = sut.isValid('valid_email@gmail.com')
 
     expect(isEmailValid).toBe(true)
   })
 
-  test('should return false if Validator returns is false', () => {
+  test('Should return false if Validator returns is false', () => {
     const sut = makeSut()
     validator.isEmailValid = false
     const isEmailValid = sut.isValid('invalid_email@gmail')
 
     expect(isEmailValid).toBe(false)
+  })
+
+  test('Should call validator with correct email', () => {
+    const sut = makeSut()
+
+    sut.isValid('valid_email@gmail.com')
+
+    expect(validator.email).toBe('valid_email@gmail.com')
   })
 })
