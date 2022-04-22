18cc56417383aad260a50af7e98a73b935b25425
Maycon Queiroz <fmaycon@gmail.com>
2022-04-22 10:09:11 -0300

    ensure AuthUseCase throw MissingParamError if no  password is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -5,6 +5,10 @@ class AuthUseCase {
     if (!email) {
       throw new MissingParamError('email')
     }
+
+    if (!password) {
+      throw new MissingParamError('password')
+    }
     return 'accessToken'
   }
 }
@@ -14,5 +18,12 @@ describe('Auth UseCase', () => {
     const promise = sut.auth()
 
     expect(promise).rejects.toThrow(new MissingParamError('email'))
+  })
+
+  test('Should Throw if no password is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth('any_email@gmail.com')
+
+    expect(promise).rejects.toThrow(new MissingParamError('password'))
   })
 })
