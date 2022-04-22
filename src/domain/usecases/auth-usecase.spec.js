c50bcf212d523487ea08738e625e5cd48d6a6d60
Maycon Queiroz <fmaycon@gmail.com>
2022-04-22 09:48:36 -0300

    ensure AuthUseCase return Throw if no email is provided

---
+++ src/domain/usecases/auth-usecase.spec.js
@@ -0,0 +1,16 @@
+class AuthUseCase {
+  async auth (email, password) {
+    if (!email) {
+      throw new Error()
+    }
+    return 'accessToken'
+  }
+}
+describe('Auth UseCase', () => {
+  test('Should Throw if no email is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth()
+
+    expect(promise).rejects.toThrow()
+  })
+})
