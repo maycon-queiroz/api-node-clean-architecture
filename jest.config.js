e74868d7ced05598444df4b45a6554916c394469
Maycon Queiroz <fmaycon@gmail.com>
2022-08-28 16:44:23 -0300

    add ignore module main in coverage

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  collectCoverageFrom: ['**/src/**/*.js'],
+  collectCoverageFrom: ['**/src/**/*.js', '!**/src/main/**.js'],
   coverageProvider: 'v8',
   verbose: true
 }
