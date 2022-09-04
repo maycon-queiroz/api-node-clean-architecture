ed696d3f7b2bd85566f533e18e6193094409ecf6
Maycon Queiroz <fmaycon@gmail.com>
2022-09-04 20:42:18 -0300

    add promise in index.js to start server

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,10 @@
-const app = require('./config/app')
+const MongoHelper = require('../infra/helper/mong-helper')
 const env = require('./config/env')
 
-app.listen(env.port, () =>
-  console.log(`Server running on http://localhost:${env.port}`)
-)
+MongoHelper.connect(env.mongoUrl).then(_ => {
+  const app = require('./config/app')
+
+  app.listen(env.port, () =>
+    console.log(`Server running on http://localhost:${env.port}`)
+  )
+})
